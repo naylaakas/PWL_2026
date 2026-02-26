0c83149405530815b30f0e7bf2717a1584fb5c98
naylaakas <naylakas05@gmail.com>
2026-02-26 15:53:52 +0700

    prak 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return 'Nama: Nayla Akas Oktavia, NIM: 244107020038';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
-
-Route::get('/hello', function () {
-    return 'Hello';
-});
-
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/about', function () {
-    return 'NIM: 244107020038, Nama: Nayla Akas Oktavia';
-});
-
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya '.$name;
-});
-
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-});
-
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
